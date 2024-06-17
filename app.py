4b4228d7f9687f141de27432280a8afcaeb5289e
Luci12 <mihalikova.lucia@yahoo.com>
2024-06-17 23:05:15 +0200

    Initial

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+users = {
+    'john': 'password123',
+    'jane': 'mypassword'
+}
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            return redirect(url_for('welcome', username=username))
+        else:
+            error = "Invalid username or password"
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
